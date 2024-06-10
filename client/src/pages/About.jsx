920f6382994ae1ea6ecc4d22f968b3c8f4c46e9b
Antoni Golachowski <91195241+AntekGol@users.noreply.github.com>
2024-06-10 20:00:35 +0200

    blog
    blog

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,19 +1,16 @@
-import React, { Fragment, useState, useEffect} from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Menu from './Menu';
 import Footer from './Footer';
 import axios from 'axios';
-
+import Blog from './Blog';
 
 function About() {
   const [parks, setParks] = useState([]);
+  const [selectedPark, setSelectedPark] = useState(null);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // const url = new URL('http://localhost:5173/api/parks');
-        // const response = await fetch(url);
-        // const data = await response.text();
-        // console.log('fetchData', data);
-        // setParks(response.data);
         const response = await axios.get('http://localhost:3001/api/parks');
         console.log(response.data);
         setParks(response.data);
@@ -23,62 +20,62 @@ function About() {
     };
     fetchData();
   }, []);
+
+  const handleBlogClick = (park) => {
+    setSelectedPark(park);
+  };
+
+  if (selectedPark) {
+    return <Blog park={selectedPark} />;
+  }
+
   return (
     <Fragment>
       <body>
         <Menu />
-        <br/><br/><br/>
+        <br /><br /><br />
         <div className="container">
-
-          <h1 className="my-4">Wszystkie parki we Wroclawiu
-          </h1>
-          {
-            parks.map((park) => {
-              const {description, location, name, photo} = park;
-              console.log(park);
+          <h1 className="my-4">Wszystkie parki we Wroclawiu</h1>
+          {parks.map((parkItem) => {
+            return parkItem.Parki.map((park) => {
+              const { nazwa_parku, opis_parku, zdjecia_parku } = park;
+              console.log("test", park);
+              console.log("test des", opis_parku);
               return (
-                <div className="park">
-              <div className="row" key={park._id} >
-              <div className="col-md-7">
-                <a href="/parki/szczytnicki">
-                  <img className="img-fluid rounded mb-3 mb-md-0" src={photo} alt="" style={{width:700, height:300}}/>
-                </a>
-              </div>
-              <div className="col-md-5">
-                <h3><img src="assets/images/tree.jpg" style={{width:45, height:45}}></img>{name}</h3>
-                <p>{description}</p> <br></br>
-                <a className="btn btn-primary" href="/blog/szczytnicki">Blog</a>
-              </div>
-              </div>
-              </div>
+                <div className="park" key={parkItem._id}>
+                  <div className="row">
+                    <div className="col-md-7">
+                      <a href="#">
+                        <img
+                          className="img-fluid rounded mb-3 mb-md-0"
+                          src={zdjecia_parku[0]}
+                          alt=""
+                          style={{ width: 700, height: 300 }}
+                        />
+                      </a>
+                    </div>
+                    <div className="col-md-5">
+                      <h3>
+                        <img src="assets/images/tree.jpg" style={{ width: 45, height: 45 }} alt="tree" />
+                        {nazwa_parku}
+                      </h3>
+                      <p>{opis_parku}</p> <br />
+                      <button className="btn btn-primary" onClick={() => handleBlogClick(park)}>
+                        Blog
+                      </button>
+                    </div>
+                  </div>
+                </div>
               );
-            })
-          }
-
-              {/* <div className="App">
-      <h1>Parki we Wrocławiu</h1>
-      <ul>
-        {parks.map((park) => (
-          <li key={park._id}>
-            <h2>{park.name}</h2>
-            <p>{park.description}</p>
-            <p>{park.location}</p>
-            <img src={park.photo} alt={park.name} style={{ width: '200px', height: '150px' }} />
-          </li>
-        ))}
-      </ul>
-    </div> */}
-
-
+            });
+          })}
           <hr />
-              <hr/>
-
-
+          <hr />
         </div>
-              <Footer />
-            </body>
-          </Fragment>
-          )
+        <Footer />
+      </body>
+    </Fragment>
+  );
 }
 
-          export default About;
+export default About;
--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 
-function Blog(){
-    return(
-        <Fragment>
-            Home
-        </Fragment>
-    )
-}
+const Blog = ({ park }) => {
+  return (
+    <div className="blog">
+      <h1>{park.nazwa_parku}</h1>
+      <p>{park.opis_parku}</p>
+      <div className="images">
+        {park.zdjecia_parku.map((src, index) => (
+          <img key={index} src={src} alt={`Zdjęcie ${index + 1}`} />
+        ))}
+      </div>
+      <div className="posts">
+        {park.posty_forum.map((post, index) => (
+          <div key={index} className="post">
+            <h3>Post by {post.autor_posta}</h3>
+            <p>{post.tresc_posta}</p>
+            <p>Created: {new Date(post.data_utworzenia_posta).toLocaleString()}</p>
+            {post.data_aktualizacji_posta && (
+              <p>Updated: {new Date(post.data_aktualizacji_posta).toLocaleString()}</p>
+            )}
+            <div className="comments">
+              {post.komentarze.map((comment, commentIndex) => (
+                <div key={commentIndex} className="comment">
+                  <p>Comment by {comment.autor_komentarza}</p>
+                  <p>{comment.tresc_komentarza}</p>
+                  <p>Created: {new Date(comment.data_utworzenia_komentarza).toLocaleString()}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
